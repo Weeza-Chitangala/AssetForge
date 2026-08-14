fcf0e1f6c01f634a81b682568c0c7b97dbc93f5c
Weeza-Chitangala <chitangala@outlook.com>
2026-08-14 03:32:02 +0200

    feat(auth): protect user management routes with Spatie permissions and Gate override

--- assetforge-api/routes/api.php
+++ assetforge-api/routes/api.php
@@ -16,11 +16,15 @@ Route::prefix('v1')->group(function () {
         });
     });
 
-    // Protected API Routes
+    // Authenticated API Routes
     Route::middleware('auth:sanctum')->group(function () {
         
-        // User Administration CRUD endpoints
-        Route::apiResource('users', UserController::class);
+        // User Administration CRUD endpoints protected by Spatie permissions
+        Route::get('users', [UserController::class, 'index'])->middleware('permission:users.view');
+        Route::post('users', [UserController::class, 'store'])->middleware('permission:users.create');
+        Route::get('users/{user}', [UserController::class, 'show'])->middleware('permission:users.view');
+        Route::put('users/{user}', [UserController::class, 'update'])->middleware('permission:users.update');
+        Route::delete('users/{user}', [UserController::class, 'destroy'])->middleware('permission:users.delete');
 
         // Permission Test Route
         Route::middleware('permission:settings.manage')->get('/test-permission', function () {
